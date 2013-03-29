011502f08d4fe71510886b5c1bea9c269f0d1b89
Andrii Stas <andrii.stas@globallogic.com>
2013-03-29 16:33:09 +0200

    Modified unit tests for comparison .apk and .jar files
    Test content added too

--- unit_test_check_files.py
+++ unit_test_check_files.py
@@ -165,39 +165,39 @@ class CompareJavaTestSuite(unittest.TestCase):
     def test_jar_comparison(self):
         tester = AFSImageComparator("","","")
 
-        res = tester.cmp_and_process_java('unit_test_files/jar_.mf_differ_no_classes/qewl_ext.jar',
-                                          'unit_test_files/jar_.mf_differ_no_classes/qewl_loc.jar')
+        res = tester.compare_and_process_java('unit_test_files/jar_.list_same_data_differ/qewl_ext.jar',
+                                              'unit_test_files/jar_.list_same_data_differ/qewl_loc.jar')
         self.assertFalse(res)
 
-        res = tester.cmp_and_process_java('unit_test_files/jar_.mf_same_no_classes/qewl_ext_same.jar',
-                                          'unit_test_files/jar_.mf_same_no_classes/qewl_loc_same.jar')
+        res = tester.compare_and_process_java('unit_test_files/jar_.list_same_data_same/qewl_ext_same.jar',
+                                              'unit_test_files/jar_.list_same_data_same/qewl_loc_same.jar')
         self.assertTrue(res)
 
-        res = tester.cmp_and_process_java('unit_test_files/jar_.classes_differ_no_mf/apache-xml_ext.jar',
-                                          'unit_test_files/jar_.classes_differ_no_mf/apache-xml_loc.jar')
+        res = tester.compare_and_process_java('unit_test_files/jar_.file_sha_differ/apache-xml_ext.jar',
+                                              'unit_test_files/jar_.file_sha_differ/apache-xml_loc.jar')
         self.assertFalse(res)
 
-        res = tester.cmp_and_process_java('unit_test_files/jar_.classes_same_no_mf/apache-xml_ext_same.jar',
-                                          'unit_test_files/jar_.classes_same_no_mf/apache-xml_loc_same.jar')
+        res = tester.compare_and_process_java('unit_test_files/jar_.file_sha_same/apache-xml_ext_same.jar',
+                                              'unit_test_files/jar_.file_sha_same/apache-xml_loc_same.jar')
         self.assertTrue(res)
 
     def test_apk_comparison(self):
         tester = AFSImageComparator("","","")
 
-        res = tester.cmp_and_process_java('unit_test_files/apk_.mf_differ_classes_differ/Browser_ext.apk',
-                                         'unit_test_files/apk_.mf_differ_classes_differ/Browser_loc.apk')
+        res = tester.compare_and_process_java('unit_test_files/apk_.sha_differ/Browser_ext.apk',
+                                              'unit_test_files/apk_.sha_differ/Browser_loc.apk')
         self.assertFalse(res)
 
-        res = tester.cmp_and_process_java('unit_test_files/apk_.mf_same_classes_same/Browser_ext_same.apk',
-                                          'unit_test_files/apk_.mf_same_classes_same/Browser_loc_same.apk')
+        res = tester.compare_and_process_java('unit_test_files/apk_.sha_same/Browser_ext_same.apk',
+                                              'unit_test_files/apk_.sha_same/Browser_loc_same.apk')
         self.assertTrue(res)
 
-        res = tester.cmp_and_process_java('unit_test_files/apk_.mf_length_differ/DemoMode_ext.apk',
-                                          'unit_test_files/apk_.mf_length_differ/DemoMode_loc.apk')
+        res = tester.compare_and_process_java('unit_test_files/apk_.list_length_differ/DemoMode_ext.apk',
+                                              'unit_test_files/apk_.list_length_differ/DemoMode_loc.apk')
         self.assertFalse(res)
 
-        res = tester.cmp_and_process_java('unit_test_files/apk_.mf_length_same/DemoMode_ext_same.apk',
-                                          'unit_test_files/apk_.mf_length_same/DemoMode_loc_same.apk')
+        res = tester.compare_and_process_java('unit_test_files/apk_.list_length_same/DemoMode_ext_same.apk',
+                                              'unit_test_files/apk_.list_length_same/DemoMode_loc_same.apk')
         self.assertTrue(res)
 
 
